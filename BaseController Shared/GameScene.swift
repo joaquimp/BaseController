b9df5a860293ef18fd0b688164cf86253a2f7a54
thallissousa <thallissousa@outlook.com>
2022-02-14 17:46:24 -0300

    Novo monstro e dinâmica de corrida
    - Adição de um novo personagem à tela;
    - Novo personagem executa um movimento da esquerda para a direita com uma velocidade fixa;
    - Movimentação conjunta dos personagens;
    - Alteração da ação de mudança dos personagem (action) ao mesmo tempo.

--- BaseController Shared/GameScene.swift
+++ BaseController Shared/GameScene.swift
@@ -4,19 +4,24 @@
 //
 //  Created by Pedro Cacique.
 //  Contributor Joaquim Pessoa Filho
-//
+//  Learning modificataions Thallis Sousa
 
 import SpriteKit
 import GameController
 
 class GameScene: SKScene, JoystickDelegate {
-
+    
     var animal = SKSpriteNode(imageNamed: "parrot")
+    var secondAnimal = SKSpriteNode(imageNamed: "penguin")
+    let initialPositionSecondAnimal = CGPoint(x: 300, y: 300)
     let initialPosition = CGPoint(x: 100, y: 100)
     let multiplier: CGFloat = 10.0
     let joystickController: JoystickController = JoystickController()
     var lastActionTime: TimeInterval = TimeInterval.zero
     let whaitForNextAction: Double = 1
+    
+    var animalDx: CGFloat = 0
+    var animalDy: CGFloat = 0
     
     class func newGameScene() -> GameScene {
         let scene = GameScene()
@@ -33,57 +38,91 @@ class GameScene: SKScene, JoystickDelegate {
     
     func createAnimal() {
         let animalName = ["parrot", "bear", "buffalo", "chick"]
+        let secondAnimalName = ["penguin", "pig", "crocodile", "narwhal"]
+        let secondIndex = Int.random(in: 0..<secondAnimalName.count)
         let index = Int.random(in: 0..<animalName.count)
         
         animal.removeFromParent()
         animal = SKSpriteNode(imageNamed: animalName[index])
         animal.position = initialPosition
         
+        secondAnimal.removeFromParent()
+        secondAnimal = SKSpriteNode(imageNamed: secondAnimalName[secondIndex])
+        secondAnimal.position = initialPositionSecondAnimal
+        
         self.addChild(animal)
+        self.addChild(secondAnimal)
     }
     
     func moveAnimal(dx: CGFloat, dy: CGFloat) {
         var xValue = animal.position.x + dx * multiplier
         var yValue = animal.position.y + dy * multiplier
         
-        if xValue > self.size.width {
-            xValue = 0
-        }
-        if xValue < 0 {
-            xValue = self.size.width
-        }
-        if yValue > self.size.height {
-            yValue = 0
-        }
-        if yValue < 0 {
-            yValue = self.size.height
-        }
+        let halfWidth = animal.size.width/2
+        let halfHeight = animal.size.height/2
         
+        if xValue > self.size.width - halfWidth {
+            xValue = self.size.width - halfWidth
+        }
+        if xValue < halfWidth {
+            xValue = halfWidth
+        }
+        if yValue > self.size.height - halfHeight {
+            yValue = self.size.height - halfHeight
+        }
+        if yValue < halfHeight {
+            yValue = halfHeight
+        }
         animal.position = CGPoint(x: xValue, y: yValue)
     }
     
+    func moveSecondAnimal(dx: CGFloat, dy: CGFloat) {
+        var xValueSAnimal = secondAnimal.position.x + dx * multiplier
+        var yValueSAnimal = secondAnimal.position.y + dy * multiplier
+        
+        let halfSecondWidth = secondAnimal.size.width/2
+        let halfSecondHeight = secondAnimal.size.height/2
+        
+        if xValueSAnimal > self.size.width - halfSecondWidth {
+            xValueSAnimal = self.size.width - halfSecondWidth
+        }
+        if xValueSAnimal < halfSecondWidth {
+            xValueSAnimal = halfSecondWidth
+        }
+        if yValueSAnimal > self.size.height - halfSecondHeight {
+            yValueSAnimal = self.size.height - halfSecondHeight
+        }
+        if yValueSAnimal < halfSecondHeight {
+            yValueSAnimal = halfSecondHeight
+        }
+        secondAnimal.position = CGPoint(x: xValueSAnimal + 1.7, y: yValueSAnimal + 1.7)
+    }
     func resetAnimal() {
         animal.position = initialPosition
+        secondAnimal.position = initialPositionSecondAnimal
+        
         createAnimal()
     }
     
     override func update(_ currentTime: TimeInterval) {
         joystickController.update(currentTime)
+        moveAnimal(dx: animalDx, dy: animalDy)
+        moveSecondAnimal(dx: animalDx, dy: animalDy)
     }
     
-    #if os(watchOS)
+#if os(watchOS)
     override func sceneDidLoad() {
         self.setUpScene()
     }
-    #else
+#else
     override func didMove(to view: SKView) {
         self.setUpScene()
     }
-    #endif
+#endif
     
     //MARK :- iOS and tvOS
 #if os(iOS) || os(tvOS)
-// Touch-based event handling
+    // Touch-based event handling
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {}
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {}
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {}
@@ -91,7 +130,7 @@ class GameScene: SKScene, JoystickDelegate {
 #endif
     
 #if os(OSX)
-// Mouse-based event handling
+    // Mouse-based event handling
     override func mouseDown(with event: NSEvent) {}
     override func mouseDragged(with event: NSEvent) {}
     override func mouseUp(with event: NSEvent) {}
@@ -120,28 +159,42 @@ class GameScene: SKScene, JoystickDelegate {
     
     func buttonPressed(command: GameCommand) {
         print("pressed: \(command)")
-        var dx:CGFloat = 0
-        var dy:CGFloat = 0
+        //        var dx:CGFloat = 0
+        //        var dy:CGFloat = 0
         
         switch command {
         case .UP:
-            dy = 1
+            animalDy = 1
         case .DOWN:
-            dy = -1
+            animalDy = -1
         case .RIGHT:
-            dx = 1
+            animalDx = 1
         case .LEFT:
-            dx = -1
+            animalDx = -1
         case .ACTION:
             resetAnimal()
             return
         }
         
-        self.moveAnimal(dx: dx, dy: dy)
+        //        self.moveAnimal(dx: dx, dy: dy)
     }
     
     func buttonReleased(command: GameCommand) {
         print("released: \(command)")
+        
+        switch command {
+        case .UP:
+            animalDy = 0
+        case .DOWN:
+            animalDy = 0
+        case .LEFT:
+            animalDx = 0
+        case .RIGHT:
+            animalDx = 0
+        case .ACTION:
+            resetAnimal()
+            return
+        }
     }
     
     func joystickUpdate(_ currentTime: TimeInterval){
